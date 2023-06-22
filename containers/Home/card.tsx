520c55c27fd405bbe0b9b593e50e6269268a772e
wechanon <wechanon2001@gmail.com>
2023-06-22 17:53:40 +0700

    wechanon5

---
+++ containers/Home/card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return (
+      <div > 
+      <h2 className=" text-red-700">wowo</h2>
+      </div>
+      
+    )
+  }
---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { SSRProvider } from "react-bootstrap";
+import '../../assets/scss/globals.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    //<SSRProvider>
+      <React.Fragment>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <title>TW-Test</title>
+        </Head>
+          <Component {...pageProps} />
+      </React.Fragment>
+    //</SSRProvider >
+  )
+}
