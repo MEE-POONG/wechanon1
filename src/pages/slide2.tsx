8bf38dc42c6d8a18729a6212a710be4f11ae368d
wechanon <wechanon2001@gmail.com>
2023-06-27 11:44:49 +0700

    wechanon23

--- src/pages/slide2.tsx
+++ src/pages/slide2.tsx
@@ -13,15 +13,16 @@ export default class SimpleSlider extends Component {
     };
     return (
       <div>
-        <h2> </h2>
+        <h2 className="mx-20 mt-8 text-3xl font-bold tracking-tight text-blue-400 sm:text-3xl">Expectation</h2>
+        
         <Slider {...settings}>
-          <div className=" mx-20 font-semibold text-2xl flex ">
+          <div className="mx-7 md:mx-20 font-semibold text-xl md:text-2xl flex">
             <h3>เรียนรู้คำว่า งาน ของจริงในการทำงาน</h3>
           </div>
-          <div className=" mx-20 font-semibold text-2xl flex ">
+          <div className="mx-10 md:mx-20 font-semibold text-xl md:text-2xl flex">
             <h3>เรียนรู้ผู้อื่น รู้จักสังคมการทำงาน</h3>
           </div>
-          <div className=" mx-20 font-semibold text-2xl flex ">
+          <div className="mx-10 md:mx-20 font-semibold text-xl md:text-2xl flex">
             <h3>เรียนรู้อัพสกิลที่ใช้ในการทำงานจริง</h3>
           </div>
         </Slider>
